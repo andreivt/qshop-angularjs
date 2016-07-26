915766c6806b26af9431469365bdb53dabe4012e
andrei <andrei.tertiscu@gmail.com>
2016-07-26 13:14:31 +0300

    bla

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
 var qshop = angular.module("qshop", ['ui.router']);
 
 
-qshop.controller("MainController", function($scope, ProductsRepository) {
-    $scope.name = "world";
-    ProductsRepository.getProductsList().then(function(result){
-      $scope.products = result.data;
+qshop.controller("MainController", function($scope, $stateParams, ProductsRepository) {
 
-      var newProd = {
-          "picture": "../assets/images/products/product-4.jpg",
-          "name": "Produs de produs",
-          "price": 12,
-          "onSale": false,
-          "description": "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis, massa fringilla consequat blandit, mauris ligula porta nisi, non tristique enim sapien vel nisl. Suspendisse vestibulum lobortis dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent nec tempus nibh. Donec mollis commodo metus et fringilla. Etiam venenatis, diam id adipiscing convallis, nisi eros lobortis tellus, feugiat adipiscing ante ante sit amet dolor. Vestibulum vehicula scelerisque facilisis. Sed faucibus placerat bibendum. Maecenas sollicitudin commodo justo, quis hendrerit leo consequat ac. Proin sit amet risus sapien, eget interdum dui. Proin justo sapien, varius sit amet hendrerit id, egestas quis mauris."
-      };
-      $scope.products.push(newProd);
-      
-      console.log('Products', result.data);
-    }, function(error){
-      console.error(error);
+    $scope.loadProduct = function() {
+        ProductsRepository.getProductsList().then(function(result) {
+            var productId = $stateParams.id;
+            for (var i = 0; i < result.data.length; i++) {
+                if (result.data[i].id == productId) {
+                    $scope.product = result.data[i];
+                    break;
+                }
+            }
+        });
+    }
+
+    ProductsRepository.getProductsList().then(function(result) {
+        $scope.products = result.data;
+
+        var newProd = {
+            "picture": "../assets/images/products/product-4.jpg",
+            "name": "Produs de produs",
+            "price": 12,
+            "onSale": true,
+            "description": "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sagittis, massa fringilla consequat blandit, mauris ligula porta nisi, non tristique enim sapien vel nisl. Suspendisse vestibulum lobortis dapibus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Praesent nec tempus nibh. Donec mollis commodo metus et fringilla. Etiam venenatis, diam id adipiscing convallis, nisi eros lobortis tellus, feugiat adipiscing ante ante sit amet dolor. Vestibulum vehicula scelerisque facilisis. Sed faucibus placerat bibendum. Maecenas sollicitudin commodo justo, quis hendrerit leo consequat ac. Proin sit amet risus sapien, eget interdum dui. Proin justo sapien, varius sit amet hendrerit id, egestas quis mauris."
+        };
+        $scope.products.push(newProd);
+
+        console.log('Products', result.data);
+    }, function(error) {
+        console.error(error);
     });
 
 });
+
+// qshop.controller("ProductController", function($scope, $routeParams) {
+//     console.log($routeParams);
+// });
+
+qshop.config(["$locationProvider", function($locationProvider) {
+  $locationProvider.html5Mode(true);
+}]);
 
 qshop.config(function($stateProvider, $urlRouterProvider) {
     //
@@ -52,6 +72,10 @@ qshop.config(function($stateProvider, $urlRouterProvider) {
         .state('register', {
             url: "/register",
             templateUrl: "templates/register.html"
+        })
+        .state('product', {
+            url: "/product/:id",
+            templateUrl: "templates/product.html"
         });
 });
 
